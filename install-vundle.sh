87783cf6bea9af33acfd90edf80ced2dc479ca20
Paweł Lenart <pawel.lenart@gmail.com>
2015-01-05 18:54:22 +0100

    Change filenames to dot-filenames Change install script to generic

--- install-vundle.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
---
+++ scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# install Base16
+mkdir ~/.config
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+# install Vundler
+mkdir -p ~/.vim/bundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
