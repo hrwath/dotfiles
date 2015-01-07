34ca406f5ac5020f220218cf433b4ba2c42546c2
Paweł Lenart <pawel.lenart@gmail.com>
2015-01-07 18:13:10 +0100

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 # install Base16
-mkdir ~/.config
+mkdir -p ~/.config
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
 # install Vundler
 mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# update submodules
+git submodule init
+git submodule update
 
 # install the_silver_searcher
 brew install the_silver_searcher
